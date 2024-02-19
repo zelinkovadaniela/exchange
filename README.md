fe97f3d29731db1325665d98612f3cbdcca60042
Daniela Zelinkova <danielazelinkova@Daniela--MacBook-Air.local>
2024-02-19 21:31:20 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Exchange service
+Microservice for collecting and providing data related to exchange rates.
+It's main purpose should be providing API for exchange rates.
+
+### How to run the development environment
+1. `mvn clean install`
+2. `mvn spring-boot:run`
+
+
+
--- src/main/java/com/app/exchange/controller/ExchangeController.java
+++ src/main/java/com/app/exchange/controller/ExchangeController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +18,7 @@ public class ExchangeController {
 
     private final ExchangeService exchangeService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public ResponseEntity<List<ExchangeRateDto>> getExchangeRates(@RequestParam(required = false) final boolean usedb) {
         try {
